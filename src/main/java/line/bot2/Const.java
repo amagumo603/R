2637ba3c629b3f08354b1dd0bff19a3bbf648e69
amagumo <test@mail.com>
2019-09-22 20:42:04 +0900

    tes

--- src/main/java/line/bot2/Const.java
+++ src/main/java/line/bot2/Const.java
@@ -1,31 +1,37 @@
 package line.bot2;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
 public class Const {
-//
-//	// アプリケーション名 (任意)
-//	static final String APPLICATION_NAME = "spreadsheet-application-example";
-//
-//	// アカウント
-//	static final String ACCOUNT_P12_ID = "hamsyatyo@amagumo.iam.gserviceaccount.com";
-//	static final File P12FILE = new File("src/main/resources/Amagumo-a21c67f1c2f0.p12");
-//
-//
-//	// 認証スコープ
-//	static final List<String> SCOPES = Arrays.asList(
-//	  "https://docs.google.com/feeds",
-//	  "https://spreadsheets.google.com/feeds");
-//
-//	// Spreadsheet API URL
-//	static final String SPREADSHEET_URL = "https://spreadsheets.google.com/feeds/spreadsheets/private/full";
-//
-//	static final URL SPREADSHEET_FEED_URL;
-//
-//	static {
-//	  try {
-//	    SPREADSHEET_FEED_URL = new URL(SPREADSHEET_URL);
-//	  } catch (MalformedURLException e) {
-//	    throw new RuntimeException(e);
-//	  }
-//	}
+
+	// アプリケーション名 (任意)
+	static final String APPLICATION_NAME = "spreadsheet-application-example";
+
+	// アカウント
+	static final String ACCOUNT_P12_ID = "hamsyatyo@amagumo.iam.gserviceaccount.com";
+	static final File P12FILE = new File("src/main/resources/Amagumo-a21c67f1c2f0.p12");
+
+
+	// 認証スコープ
+	static final List<String> SCOPES = Arrays.asList(
+	  "https://docs.google.com/feeds",
+	  "https://spreadsheets.google.com/feeds");
+
+	// Spreadsheet API URL
+	static final String SPREADSHEET_URL = "https://spreadsheets.google.com/feeds/spreadsheets/private/full";
+
+	static final URL SPREADSHEET_FEED_URL;
+
+	static {
+	  try {
+	    SPREADSHEET_FEED_URL = new URL(SPREADSHEET_URL);
+	  } catch (MalformedURLException e) {
+	    throw new RuntimeException(e);
+	  }
+	}
 
 }
