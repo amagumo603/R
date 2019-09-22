9fd3d3f22969f5e27c0922216b9b30520ea34d9a
amagumo <test@mail.com>
2019-09-22 20:45:36 +0900

    tes

--- src/main/java/line/bot2/SpreadsheetTool.java
+++ src/main/java/line/bot2/SpreadsheetTool.java
@@ -1,34 +1,41 @@
 package line.bot2;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
 public class SpreadsheetTool {
-//
-//	public static Credential authorize() throws Exception {
-//		  System.out.println("authorize in");
-//
-//		  HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-//		  JsonFactory jsonFactory = new JacksonFactory();
-//
-//		  GoogleCredential credential = new GoogleCredential.Builder()
-//		    .setTransport(httpTransport)
-//		    .setJsonFactory(jsonFactory)
-//		    .setServiceAccountId(Const.ACCOUNT_P12_ID)
-//		    .setServiceAccountPrivateKeyFromP12File(Const.P12FILE)
-//		    .setServiceAccountScopes(Const.SCOPES)
-//		    .build();
-//
-//		  boolean ret = credential.refreshToken();
-//		  // debug dump
-//		  System.out.println("refreshToken:" + ret);
-//
-//		  // debug dump
-//		  if (credential != null) {
-//		    System.out.println("AccessToken:" + credential.getAccessToken());
-//		  }
-//
-//		  System.out.println("authorize out");
-//
-//		  return credential;
-//		}
+
+	public static Credential authorize() throws Exception {
+		  System.out.println("authorize in");
+
+		  HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		  JsonFactory jsonFactory = new JacksonFactory();
+
+		  GoogleCredential credential = new GoogleCredential.Builder()
+		    .setTransport(httpTransport)
+		    .setJsonFactory(jsonFactory)
+		    .setServiceAccountId(Const.ACCOUNT_P12_ID)
+		    .setServiceAccountPrivateKeyFromP12File(Const.P12FILE)
+		    .setServiceAccountScopes(Const.SCOPES)
+		    .build();
+
+		  boolean ret = credential.refreshToken();
+		  // debug dump
+		  System.out.println("refreshToken:" + ret);
+
+		  // debug dump
+		  if (credential != null) {
+		    System.out.println("AccessToken:" + credential.getAccessToken());
+		  }
+
+		  System.out.println("authorize out");
+
+		  return credential;
+		}
 //
 ///**
 // * サービスを取得する
